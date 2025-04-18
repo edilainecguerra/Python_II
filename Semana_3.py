c89fd422b71ede71a4d3c30c2b5f9a0db4e0d6a1
edilainecguerra <161534158+edilainecguerra@users.noreply.github.com>
2025-04-18 20:23:24 -0300

    Sequência - Funções Recursivas

--- Semana_3.py
+++ Semana_3.py
@@ -17,3 +17,24 @@ l = [1,2,3]
 imprime_rec(l)
 
 
+#Para melhor compreensão da linguagem: 
+def imprime_recursivamente(lista, indice=0):
+    if indice < len(lista):
+        print(lista[indice])
+        imprime_recursivamente(lista, indice + 1)
+
+# Testando:
+lista = [10, 20, 30]
+imprime_recursivamente(lista)
+
+# Quando usar recursão: se existir uma função simples e não recursiva da função, ela deve ser usada. 
+# Usamos a recursão quando o problema pode ser definido recursivamente de forma natural. 
+# Necessário definir: 
+# 1 - Definir o problema de forma recursiva, ou seja, em termos dele mesmo
+# 2 - Definir a condição de término (ou condição básica)
+# 3 - A cada chamada recursiva, deve-se tentar garantir que se está mais próximo de satisfazer a condição de término
+
+# Exemplo: Implementar a Função Recursiva para calcular o fatorial de um número inteiro positivo passado como parâmetro:
+
+
+
